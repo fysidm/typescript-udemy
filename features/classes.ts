9127b1c1d01a7d4c870e8d5517543cd2183edeec
GeorgeWu <fysidm@gmail.com>
2021-09-01 18:05:52 +0800

    class constructor

--- features/classes.ts
+++ features/classes.ts
@@ -3,27 +3,36 @@ class Vehicle {
   //   console.log(`chugga chugga`);
   // }
 
+  // color: string = 'red';
+
+  // constructor(color: string) {
+  //   this.color = color;
+  // }
+
+  constructor(public color: string) { }
+
   protected honk(): void {
     console.log('beep');
   }
 }
 
-const vehicle = new Vehicle();
-vehicle.honk();
+const vehicle = new Vehicle('orange');
+// vehicle.honk();
+console.log(vehicle.color);
 
-class Car extends Vehicle {
-  // override method
-  // can not override public method
-  private drive(): void {
-    console.log('vroom');
-  }
+// class Car extends Vehicle {
+//   // override method
+//   // can not override public method
+//   private drive(): void {
+//     console.log('vroom');
+//   }
 
-  startDrivingProcess(): void {
-    this.drive();
-    this.honk();
-  }
-}
+//   startDrivingProcess(): void {
+//     this.drive();
+//     this.honk();
+//   }
+// }
 
-const car = new Car();
-car.startDrivingProcess();
-car.honk();
+// const car = new Car();
+// car.startDrivingProcess();
+// car.honk();
