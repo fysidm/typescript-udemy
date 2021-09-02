b88366b1260cf7c7d33428d255e16484732d2a00
GeorgeWu <fysidm@gmail.com>
2021-09-02 21:25:04 +0800

    maps prototype with user class

---
+++ maps/src/User.ts
@@ -0,0 +1,18 @@
+// install `@types/faker` fix faker do not have type definition files.
+import faker from 'faker';
+
+class User {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+}
