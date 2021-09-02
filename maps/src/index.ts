1badc90738e3ba5a1d8d1887c04afb659a6e45ad
GeorgeWu <fysidm@gmail.com>
2021-09-02 22:17:54 +0800

    Add googlemap

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,3 +1,5 @@
+// Fix google function
+/// <reference types="@types/google.maps" />
 import { User } from './User';
 import { Company } from './Company';
 
@@ -7,3 +9,7 @@ const company = new Company();
 console.log(user);
 console.log(company);
 
+new google.maps.Map(document.getElementById('map'), {
+  center: { lat: 0, lng: 0 },
+  zoom: 1,
+});
