85eafc00fbe3d0610b681d209d6933ea4b7469ce
GeorgeWu <fysidm@gmail.com>
2021-09-28 15:32:21 +0800

    Add private CustomMap

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -2,14 +2,8 @@
 /// <reference types="@types/google.maps" />
 import { User } from './User';
 import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 
 const user = new User();
 const company = new Company();
-
-console.log(user);
-console.log(company);
-
-new google.maps.Map(document.getElementById('map'), {
-  center: { lat: 0, lng: 0 },
-  zoom: 1,
-});
+new CustomMap('map');
