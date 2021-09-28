c5b6c76ec78a4ae28c06db8c3287b241e8180483
GeorgeWu <fysidm@gmail.com>
2021-09-28 15:41:29 +0800

    Duplicate code in add marker

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -18,10 +18,16 @@ export class CustomMap {
         lat: user.location.lat,
         lng: user.location.lng,
       }
-    })
+    });
   }
   // Bad Code
   addCompanyMarker(company: Company): void {
-
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: company.location.lat,
+        lng: company.location.lng,
+      }
+    });
   }
 }
