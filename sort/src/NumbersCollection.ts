86cb7bac0aae8e7f6b836f108403391914966922
GeorgeWu <fysidm@gmail.com>
2021-09-30 15:21:06 +0800

    Extract key logic into class

---
+++ sort/src/NumbersCollection.ts
@@ -0,0 +1,19 @@
+export class NumbersCollection {
+  constructor(public data: number[]) { }
+
+  // const collection = new NumbersCollection([1,2,3]);
+  // collection.length
+  get length(): number {
+    return this.data.length;
+  }
+
+  compare(leftIndex: number, rightIndex: number): boolean {
+    return this.data[leftIndex] > this.data[rightIndex];
+  }
+
+  swap(leftIndex: number, rightIndex: number): void {
+    const leftHand = this.data[leftIndex];
+    this.data[leftIndex] = this.data[rightIndex];
+    this.data[rightIndex] = leftHand;
+  }
+}
