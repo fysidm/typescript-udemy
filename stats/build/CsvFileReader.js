4773307d58c79e4caa88d4669b45ce74c67bee5a
GeorgeWu <fysidm@gmail.com>
2021-10-01 16:33:59 +0800

    Refactor with abstract and generic class

--- stats/build/CsvFileReader.js
+++ stats/build/CsvFileReader.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CsvFileReader = void 0;
 var fs_1 = __importDefault(require("fs"));
-var utils_1 = require("./utils");
+// Generic <TypeOfData> for convention <T>
 var CsvFileReader = /** @class */ (function () {
     function CsvFileReader(filename) {
         this.filename = filename;
@@ -19,18 +19,7 @@ var CsvFileReader = /** @class */ (function () {
             .map(function (row) {
             return row.split(',');
         })
-            .map(function (row) {
-            return [
-                utils_1.dateStringToDate(row[0]),
-                row[1],
-                row[2],
-                parseInt(row[3]),
-                parseInt(row[4]),
-                // Type assertion => 'H', 'A', 'D'
-                row[5],
-                row[6],
-            ];
-        });
+            .map(this.mapRow);
     };
     return CsvFileReader;
 }());
--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -1,14 +1,12 @@
 import fs from 'fs';
-import { dateStringToDate } from './utils';
-import { MatchResult } from './MatchResult';
 
-// Tuple
-type MatchData = [Date, string, string, number, number, MatchResult, string];
-
-export class CsvFileReader {
-  data: MatchData[] = [];
+// Generic <TypeOfData> for convention <T>
+export abstract class CsvFileReader<T> {
+  data: T[] = [];
 
   constructor(public filename: string) { }
+
+  abstract mapRow(row: string[]): T;
 
   read(): void {
     this.data = fs.readFileSync(this.filename, {
@@ -18,17 +16,6 @@ export class CsvFileReader {
       .map((row: string): string[] => {
         return row.split(',');
       })
-      .map((row: string[]): MatchData => {
-        return [
-          dateStringToDate(row[0]),
-          row[1],
-          row[2],
-          parseInt(row[3]),
-          parseInt(row[4]),
-          // Type assertion => 'H', 'A', 'D'
-          row[5] as MatchResult,
-          row[6],
-        ];
-      });
+      .map(this.mapRow);
   }
 }
