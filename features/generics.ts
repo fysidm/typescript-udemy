d19d03c0ffaf2bf2625e13a2252609c5f6ba2761
GeorgeWu <fysidm@gmail.com>
2021-10-05 21:33:30 +0800

    function generics

--- features/generics.ts
+++ features/generics.ts
@@ -24,3 +24,25 @@ class ArrayOfAnything<T> {
 }
 
 const arr = new ArrayOfAnything(['a', 'b', 'c', 'd']);
+
+// Example of generics with functions
+
+function printStrings(arr: string[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printNumbers(arr: number[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printAnything<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+printAnything(['a', 'b', 'c']);
