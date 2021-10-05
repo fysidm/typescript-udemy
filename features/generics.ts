2be466b044b10b2f0e79a60eb302c2a6c6c02db0
GeorgeWu <fysidm@gmail.com>
2021-10-05 21:38:30 +0800

    Generic Constraints

--- features/generics.ts
+++ features/generics.ts
@@ -46,3 +46,30 @@ function printAnything<T>(arr: T[]): void {
 }
 
 printAnything(['a', 'b', 'c']);
+
+// Generic Constraints
+class Car {
+  print() {
+    console.log('I am a car');
+  }
+}
+
+class House {
+  print() {
+    console.log('I am a house');
+  }
+}
+
+interface Printable {
+  print(): void;
+}
+
+function printHousesOrCars<T extends Printable>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print();
+  }
+}
+
+printHousesOrCars([new House(), new Car()])
+printHousesOrCars<Car>([new Car(), new Car()])
+printHousesOrCars<House>([new House(), new House()])
