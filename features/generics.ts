e801559fb15eadb26cd23d4234f5dc309c24c5d6
GeorgeWu <fysidm@gmail.com>
2021-10-05 21:18:27 +0800

    Generics have type inference

---
+++ features/generics.ts
@@ -0,0 +1,26 @@
+class ArrayOfNumbers {
+  constructor(public collection: number[]) { }
+
+  get(index: number): number {
+    return this.collection[index];
+  }
+}
+
+class ArrayOfStrings {
+  constructor(public collection: string[]) { }
+
+  get(index: number): string {
+    return this.collection[index];
+  }
+}
+
+// Using generics
+class ArrayOfAnything<T> {
+  constructor(public collection: T[]) { }
+
+  get(index: number): T {
+    return this.collection[index];
+  }
+}
+
+const arr = new ArrayOfAnything(['a', 'b', 'c', 'd']);
