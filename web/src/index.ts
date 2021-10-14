951a63c33e8809ea6628bc9fce0f7c92294683b3
GeorgeWu <fysidm@gmail.com>
2021-10-14 15:31:26 +0800

    Add set function with optional interface properties

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,5 +2,9 @@ import { User } from './models/User';
 
 const user = new User({ name: 'myname', age: 20 });
 
+user.set({ name: 'newname', age: 9999 });
+user.set({ name: 'newname2' });
+user.set({ age: 99 });
+
 console.log(user.get('name'));
 console.log(user.get('age'));
