bddda5bf27bb57494af133de3644fbcaf25c363b
GeorgeWu <fysidm@gmail.com>
2021-10-14 16:01:02 +0800

    Add trigger of events

--- web/src/index.ts
+++ web/src/index.ts
@@ -3,13 +3,15 @@ import { User } from './models/User';
 const user = new User({ name: 'myname', age: 20 });
 
 user.on('change', () => {
-
+  console.log('change1');
 });
 user.on('change', () => {
-
+  console.log('change2');
 });
-user.on('asdfasdf', () => {
-
+user.on('save', () => {
+  console.log('save was triggered');
 });
 
-console.log(user);
+user.trigger('change');
+user.trigger('save');
+user.trigger('dafad');
