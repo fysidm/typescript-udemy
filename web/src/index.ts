01a35454cb6d18916a24864316d0c2b086741adc
GeorgeWu <fysidm@gmail.com>
2023-02-17 22:46:31 +0800

    Accessor reminder

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,7 +2,7 @@ import { SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION } from 'constants';
 import { User } from './models/User';
 
 // Add new user
-const user = new User({ name: 'new record', age: 1 });
+// const user = new User({ name: 'new record', age: 1 });
 
 // Update user
 // const user = new User({ id: 1 });
@@ -11,8 +11,22 @@ const user = new User({ name: 'new record', age: 1 });
 // user.save();
 
 // test events
-user.events.on('change', () => {
-  console.log('change');
-})
+// user.events.on('change', () => {
+//   console.log('change');
+// })
 
-user.events.trigger('change');
+// user.events.trigger('change');
+
+// A quick reminder on accessors
+class Person {
+  constructor(public firstName: string, public lastName: string) { }
+
+  // get fullName(): string {
+  fullName(): string {
+    return `${this.firstName} ${this.lastName}`
+  }
+}
+
+const person = new Person('first', 'last');
+// console.log(person.fullName)
+console.log(person.fullName())
