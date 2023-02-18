105f290b84a395bace20d562743d6dde10fea86d
GeorgeWu <fysidm@gmail.com>
2023-02-18 12:05:22 +0800

    Shortened passthrough methods

--- web/src/models/Model.ts
+++ web/src/models/Model.ts
@@ -27,17 +27,10 @@ export class Model<T extends MayHasId> {
     private sync: Sync<T>,
   ) { }
 
-  get on() {
-    return this.events.on;
-  }
-
-  get trigger() {
-    return this.events.trigger;
-  }
-
-  get get() {
-    return this.attributes.get;
-  }
+  // only do this when properties in constructor
+  on = this.events.on;
+  trigger = this.events.trigger;
+  get = this.attributes.get;
 
   set(update: T): void {
     this.attributes.set(update);
