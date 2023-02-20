72e9f0e74db936fe705a0135fe59d66d27a2bdb1
GeorgeWu <fysidm@gmail.com>
2023-02-20 09:27:30 +0800

    Bind event to class name and add method to model

--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -5,17 +5,16 @@ export class UserForm {
 
   eventsMap(): { [key: string]: () => void } {
     return {
-      'click:button': this.onButtonClick,
-      'mouseenter:h1': this.onHeadHover,
+      'click:.set-age': this.onSetAgeClick,
     };
   }
 
-  onHeadHover(): void {
-    console.log('H1 was hovered over');
-  }
-
-  onButtonClick(): void {
-    console.log('Hi there');
+  // will has error of model is undefined
+  // , the solution is change to arrow function
+  // change onSetAgeClick(): void {} 
+  // to onSetAgeClick = (): void => {}
+  onSetAgeClick = (): void => {
+    this.model.setRandomAge();
   }
 
   template(): string {
@@ -25,7 +24,8 @@ export class UserForm {
       <div>User name: ${this.model.get('name')}</div>
       <div>User age: ${this.model.get('age')}</div>
       <input />
-        <button>Click Me</button>
+      <button>Click Me</button>
+      <button class="set-age">Set Random Age</button>
       </div>
     `;
   }
