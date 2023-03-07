7e49f7dd38860d46722fc1dfd431e624d957a1b4
GeorgeWu <fysidm@gmail.com>
2023-03-07 15:41:32 +0800

    Wire up session

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,13 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
 
 import { router } from './routes/loginRoutes';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(cookieSession({ keys: ['asdfa'] }));
 app.use(router);
 
 app.listen(3000, () => {
