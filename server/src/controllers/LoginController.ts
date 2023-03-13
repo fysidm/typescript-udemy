1416e88d9f068a1f7d1fc38646975eb9ea7e8530
GeorgeWu <fysidm@gmail.com>
2023-03-13 19:51:42 +0800

    Testing use decorator

--- server/src/controllers/LoginController.ts
+++ server/src/controllers/LoginController.ts
@@ -1,11 +1,17 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
 // Decorators
-import { get, controller } from './decorators';
+import { get, controller, use } from './decorators';
+
+function logger(req: Request, res: Response, next: NextFunction) {
+  console.log('Request was made!!');
+  next();
+}
 
 @controller('/auth')
 class LoginController {
   @get('/login')
+  @use(logger)
   getLogin(req: Request, res: Response): void {
     res.send(`
       <form method="POST">
