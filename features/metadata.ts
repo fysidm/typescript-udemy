4ce1eeabded85f4bd75a30cddbe1d3c8fa8e33fe
GeorgeWu <fysidm@gmail.com>
2023-03-13 15:24:14 +0800

    Simple decorator with metadata

--- features/metadata.ts
+++ features/metadata.ts
@@ -1,5 +1,22 @@
 import 'reflect-metadata';
 
+class Plane {
+  color: string = 'red';
+
+  @markFunction
+  fly(): void {
+    console.log('vrrrrrrrr');
+  }
+}
+
+function markFunction(target: Plane, key: string) {
+  Reflect.defineMetadata('secret', 123, target, key);
+}
+
+const secret = Reflect.getMetadata('secret', Plane.prototype, 'fly');
+
+console.log(secret);
+
 // metadata examples
 // const plane = {
 //   color: 'red',
