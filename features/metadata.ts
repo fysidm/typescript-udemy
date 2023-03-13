899b5098b156057e152fe9bc2f7ac9756dea60dd
GeorgeWu <fysidm@gmail.com>
2023-03-13 15:19:37 +0800

    Add metadata examples

---
+++ features/metadata.ts
@@ -0,0 +1,25 @@
+import 'reflect-metadata';
+
+// metadata examples
+// const plane = {
+//   color: 'red',
+// };
+
+// 1. metadata simple example
+// Reflect.defineMetadata('note', 'hi there', plane);
+// Reflect.defineMetadata('height', 10, plane);
+
+// console.log(plane);
+
+// const note = Reflect.getMetadata('note', plane);
+// console.log(note);
+
+// const height = Reflect.getMetadata('height', plane);
+// console.log(height);
+
+// 2. add metadata in property
+// Reflect.defineMetadata('note', 'hi there', plane, 'color');
+
+// const note = Reflect.getMetadata('note', plane, 'color');
+
+// console.log(note);
