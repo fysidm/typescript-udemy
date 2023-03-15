4b7016ed9526b43ed5b2f8dc32e10ba44edfb5cf
GeorgeWu <fysidm@gmail.com>
2023-03-15 13:07:32 +0800

    Add actions

---
+++ rrts/src/actions/index.ts
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import { Dispatch } from 'redux';
+
+import { ActionTypes } from './types';
+
+// Fetch data structure
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+// Dispatch data structure
+interface FetchTodosAction {
+  type: ActionTypes.fetchTodos;
+  payload: Todo[];
+}
+
+const url = 'https://jsonplaceholder.typicode.com/todos';
+
+export const fetchTodos = () => {
+  return async (dispatch: Dispatch) => {
+    const response = await axios.get<Todo[]>(url);
+
+    dispatch<FetchTodosAction>({
+      type: ActionTypes.fetchTodos,
+      payload: response.data,
+    });
+  };
+};
